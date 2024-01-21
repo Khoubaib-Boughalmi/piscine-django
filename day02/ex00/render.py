a2061bbd906c8ee2a4b669cfd7563d831ff751e3
khoubaib boughalmi <khoubaib.kb99@gmail.com>
2024-01-21 18:03:56 +0100

    finished ex00:

--- day02/ex00/render.py
+++ day02/ex00/render.py
@@ -8,7 +8,7 @@ def check_number_args() :
         exit(1)
 
 def check_file_extension (templateFile):
-    if (len(templateFile.split(".") != 2)):
+    if (len(templateFile.split(".")) != 2):
         print("Error: Wrong file extension format")
         exit(1)
     ext = templateFile.split(".")[1]
@@ -36,8 +36,51 @@ def check_template_file(templateFile) :
         exit(1)
     return template
 
+setting_obj = {}
+
+def parse_settings_file(settingsList):
+    global setting_obj
+    for item in settingsList:
+        key = item.split("=")[0]
+        value = item.split("=")[1]
+        setting_obj[str(key).strip()] = value.strip()
+
+def inject_values_into_template(templateList, settingsList):
+    pattern = r'\{([^}]+)\}'
+    populated_template = ""
+    
+    for line in templateList:
+        match = re.search(pattern, line)
+        if match:
+            populated_template += re.sub(pattern, setting_obj.get(match.group(1), "").replace("\"", ""), line)
+        else:
+            populated_template += line
+    return populated_template
+
+def generate_html(populated_template):
+    final_html = f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        {populated_template}
+    </body>
+    </html>
+    """
+    return final_html
 
 if __name__ == '__main__':
     check_number_args()
     check_file_extension(sys.argv[1])
-    print(check_template_file(sys.argv[1]))
+    templateList = check_template_file(sys.argv[1])
+    settingsList = check_and_get_settings_file()
+    parse_settings_file(settingsList)
+    populated_template = inject_values_into_template(templateList, settingsList)
+    print(populated_template)
+    final_html = generate_html(populated_template)
+    with open("views.html", "w") as f:
+        f.write(final_html)
--- day02/ex00/settings.py
+++ day02/ex00/settings.py
@@ -1,2 +1,2 @@
-name = "khoubaib"
+name = "mont"
 last = "boughalmi"
