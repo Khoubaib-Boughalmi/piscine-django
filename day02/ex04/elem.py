430a4670b3fbe86d4c2870c0f101e2270e196a4a
khoubaib boughalmi <khoubaib.kb99@gmail.com>
2024-01-23 02:12:50 +0100

    enough d02:

--- day02/ex04/elem.py
+++ day02/ex04/elem.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 class Text(str):
     """
     A Text class to represent a text you could use with your HTML elements.
@@ -7,23 +8,21 @@ class Text(str):
     Because directly using str class was too mainstream.
     """
 
-    def __str__(self):
-        """
-        Do you really need a comment to understand this method?..
-        """
-        return super().__str__().replace('\n', '\n<br />\n')
+    class Text(str):
+        def __str__(self):
+            return super().__str__().replace('<', '&lt;').replace('>', '&gt;').replace('"', '&quot;').replace('\n', '\n<br />\n')
 
 
-class Elem: 
+class Elem:
     """
     Elem will permit us to represent our HTML elements.
     """
 
     class ValidationError(Exception):
-        def __init__(self):
-            super().__init("Validation Error.")
+        def __init__(self) -> None:
+            super().__init__("Validation Error.")
 
-    def __init__(self, tag='div', attr={}, content=None, tag_type='double'):
+    def __init__(self, tag="div", attr={}, content=None, tag_type="double"):
         """
         __init__() method.
 
@@ -34,7 +33,14 @@ class Elem:
         self.attr = attr
         self.content = []
         self.tag_type = tag_type
-        if (not check_type())
+        if (not self.check_type(content) or content is None):
+            raise Elem.ValidationError()
+        if tag_type != "simple" and tag_type != "double":
+            raise Elem.ValidationError()
+        if type(content) == list:
+            self.content = content
+        elif type(content != None):
+            self.content.append(content)
 
     def __str__(self):
         """
@@ -43,24 +49,22 @@ class Elem:
         Make sure it renders everything (tag, attributes, embedded
         elements...).
         """
-        result = ""
+        result = f'<{self.tag}{self.__make_attr()}'
         if self.tag_type == 'double':
-            result += f"""
-                <{self.tag} {self.attr}> {self.content} </{self.tag}>
-            """
-        elif self.tag_type == 'simple':
-            result += f"""
-                <{self.tag} {self.attr} />
-            """
-        return result
+            result += '>' + self.__make_content()
+            result += f'</{self.tag}>'
 
+        elif self.tag_type == 'simple':
+            result += f'/>'
+        return result
+    
     def __make_attr(self):
         """
         Here is a function to render our elements attributes.
         """
-        result = ''
+        result = ""
         for pair in sorted(self.attr.items()):
-            result += ' ' + str(pair[0]) + '="' + str(pair[1]) + '"'
+            result += " " + str(pair[0]) + '="' + str(pair[1]) + '"'
         return result
 
     def __make_content(self):
@@ -69,18 +73,18 @@ class Elem:
         """
 
         if len(self.content) == 0:
-            return ''
-        result = '\n'
+            return ""
+        result = "\n"
         for elem in self.content:
             result += elem
         return result
 
     def add_content(self, content):
         if not Elem.check_type(content):
-            raise Elem.ValidationError
+            raise Elem.ValidationError()
         if type(content) == list:
-            self.content += [elem for elem in content if elem != Text('')]
-        elif content != Text(''):
+            self.content += [elem for elem in content if elem != Text("")]
+        elif content != Text(""):
             self.content.append(content)
 
     @staticmethod
@@ -89,21 +93,38 @@ class Elem:
         Is this object a HTML-compatible Text instance or a Elem, or even a
         list of both?
         """
-        return (isinstance(content, Elem) or type(content) == Text or
-                (type(content) == list and all([type(elem) == Text or
-                                                isinstance(elem, Elem)
-                                                for elem in content])))
-
+        return (
+            isinstance(content, Elem)
+            or type(content) == Text
+            or (
+                type(content) == list
+                and all(
+                    [type(elem) == Text or isinstance(elem, Elem) for elem in content]
+                )
+            )
+        )
 
 
 def test():
-    html = Elem('html', content=[
-                Elem('head', content=Elem(
-                    'title', content=Text('"Hello ground!"'))),
-                Elem('body', content=[Elem('h1', content=Text('"Oh no, not again!"')),
-                                      Elem('img', {'src': 'http://i.imgur.com/pfp3T.jpg'}, tag_type='simple')])])
+    html = Elem(
+        "html",
+        content=[
+            Elem("head", content=Elem("title", content=Text('"Hello ground!"'))),
+            Elem(
+                "body",
+                content=[
+                    Elem("h1", content=Text('"Oh no, not again!"')),
+                    Elem(
+                        "img",
+                        {"src": "http://i.imgur.com/pfp3T.jpg"},
+                        tag_type="simple",
+                    ),
+                ],
+            ),
+        ],
+    )
     print(html)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     [test()]
